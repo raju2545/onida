2a65745b2401cb923316a5fdf42c946aa8dce604
root <root@ip-172-31-46-183.ec2.internal>
2019-07-06 11:58:29 +0000

    a

---
+++ src/main/java/com/onida/a.java
@@ -0,0 +1,14 @@
+package com.onida;
+
+/**
+ * Hello world!
+ *
+ */
+public class a
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
